43db61de2eae0a50d93747673cfcbb417c92327e
shubhamtiwary1 <114869477+shubhamtiwary1@users.noreply.github.com>
2023-11-08 16:31:30 +0530

    Delete Assignment 3 directory

--- Assignment 3/q1/iaop.sh
+++
@@ -1,9 +0,0 @@
-echo "Enter the 1st no"
-read a
-echo "Enter the 2nd no"
-read b
-echo "$a + $b = $((a+b))"
-echo "$a - $b = $((a-b))"
-echo "$a * $b = $((a*b))"
-echo "$a / $b = $((a/b))"
-echo "$a % $b = $((a%b))"
--- Assignment 3/q3/q3.sh
+++
@@ -1,5 +0,0 @@
-echo "Enter the Ramesh salary:"
-read a
-da=$(echo "0.4*$a" | bc)
-ra=$(echo "0.2*$a" | bc)
-echo "Gross salary is : $(echo "$a + $da + $ra" | bc)"
